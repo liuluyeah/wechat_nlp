dcebea3b1c1f0a70367b6aefc7b93c3a8ebb55f9
liuluyeah <1908689923@qq.com>
2018-03-15 17:06:55 +0800

    消息撤回监听、好友数据统计（包括性别、省份、城市）

--- recallMessage.py
+++ recallMessage.py
@@ -7,13 +7,13 @@ import sys
 import time
 import re
 import codecs
+import collections,json
 # reload(sys)
 # sys.setdefaultencoding('utf8')
 import os
 
 msg_information = {}
 face_bug = None  # 针对表情包的内容
-fw = codecs.open('recall.txt','a','utf-8')
 
 @itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD,
                       MAP, SHARING, RECORDING, ATTACHMENT,
@@ -191,6 +191,59 @@ def information(msg):
             # 删除字典旧消息
             msg_information.pop(old_msg_id)
 
+
+
+
+""" 数据统计分析函数 """
+fdata = codecs.open('message.json','w','utf8')
+""" 好友地区分布 """
+def friends_area(friends):
+    provinces = []
+    cities = []
+    provinces.append([friend['Province'] for friend in friends[1:]])
+    cities.append([friend['City'] for friend in friends[1:]])
+    province = dict(collections.Counter(provinces[0]))
+    city = dict(collections.Counter(cities[0]))
+    del province[""]
+    del city[""]
+    province_b = sorted(province.items(), key=lambda province: province[1], reverse=True)
+    city_b = sorted(city.items(), key=lambda city: city[1], reverse=True)
+    p1,p2 = [],[]
+    for ele in province_b:
+        p1.append(ele[0])
+        p2.append(ele[1])
+    c1,c2 = [],[]
+    for ele in city_b:
+        c1.append(ele[0])
+        c2.append(ele[1])
+    return p1[:10], p2[:10],c1[:10],c2[:10]
+
+def friend_sex(friends):
+    male = female = other = 0
+    for i in friends[1:]:
+        sex = i["Sex"]
+        if sex == 1:
+            male += 1
+        elif sex == 2:
+            female += 1
+        else:
+            other += 1
+    total = len(friends[1:])
+    return male,female,other
+
 if __name__ == '__main__':
+    fw = codecs.open('recall.txt', 'w', 'utf-8')
     itchat.auto_login( hotReload=True)
+    friends = itchat.get_friends(update=True)[0:]
+    s0, s1, s2 = friend_sex(friends)
+    p1, p2, c1, c2 = friends_area(friends)
+    data={'s':[s0, s1 ,s2],
+          'p1': p1,
+          'p2': p2,
+          'c1': c1,
+          'c2': c2
+          }
+    json.dump(data, fdata)
     itchat.run()
+    fw.close()
+
