a9faa302f073bc48b05805d7b0d422a91549ac52
liuluyeah <1908689923@qq.com>
2018-03-20 10:34:22 +0800

    新增文智API接口使用

--- recallMessage.py
+++ recallMessage.py
@@ -1,19 +1,24 @@
 # coding:utf-8
-#!D:\Anaconda2\py2.exe
-import itchat
-from itchat.content import TEXT
-from itchat.content import *
-import sys
-import time
-import re
 import codecs
-import collections,json
-# reload(sys)
-# sys.setdefaultencoding('utf8')
+import collections
+import json
+import sys
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+except:
+    pass
 import os
+import re
+import time
 
+import itchat
+from itchat.content import *
+from secretary import analyze, ifPersonalInfo
+from wenzhi import zhengzhi, laji
 msg_information = {}
 face_bug = None  # 针对表情包的内容
+
 
 @itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD,
                       MAP, SHARING, RECORDING, ATTACHMENT,
@@ -31,16 +36,31 @@ def handle_receive_msg(msg):
     msg_share_url = None  # 储存分享的链接，比如分享的文章和音乐
     print(msg['Type'])
     print(msg['MsgId'])
+
     # 如果发送的消息是文本或者好友推荐
     if msg['Type'] == 'Text' or msg['Type'] == 'Friends':
         msg_content = msg['Text']
         print(msg_content)
+        r1 = float(zhengzhi(msg_content))
+        r2 = float(laji(msg_content))
+        if r1 > 0.5:
+            print(msg_content + '--政治敏感度：'+ str(r1))
+        if r2 > 0.5:
+            print(msg_content + '--垃圾敏感度：'+ str(r2))
+        if (msg['FromUserName'] != itchat.originInstance.storageClass.userName) and (analyze(msg_content, 0)):
+            # print(msg_from)
+            itchat.send(u'您可能在和“%s”的聊天中被询问私人信息，请注意防范 聊天内容为：\n\n%s' %
+                        (msg_from, msg['Text']), toUserName='filehelper')
+        elif (msg['FromUserName'] == itchat.originInstance.storageClass.userName) and (ifPersonalInfo(msg_content)):
+            itchat.send(u'您可能在和“%s”的聊天中泄露了私人信息，请注意防范 聊天内容为：\n\n%s\n\n请及时撤回' %
+                        (msg_from, msg['Text']),toUserName='filehelper')
     # 如果发送的消息是附件、视屏、图片、语音
     elif msg['Type'] == "Attachment" or msg['Type'] == "Video" \
             or msg['Type'] == 'Picture' \
             or msg['Type'] == 'Recording':
         msg_content = msg['FileName']  # 内容就是他们的文件名
         msg['Text'](str(msg_content))  # 下载文件
+
     #将信息存储在字典中，每一个msg_id对应一条信息
     msg_information.update(
         {
@@ -53,6 +73,7 @@ def handle_receive_msg(msg):
         }
     )
 
+
 ##这个是用于监听是否有friend消息撤回
 @itchat.msg_register(NOTE, isFriendChat=True, isGroupChat=True, isMpChat=True)
 def information(msg):
@@ -60,6 +81,7 @@ def information(msg):
     if '撤回了一条消息' in msg['Content']:
         # 在返回的content查找撤回的消息的id
         old_msg_id = re.search("\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
+
         # 得到消息
         old_msg = msg_information.get(old_msg_id)
         print(old_msg)
@@ -74,8 +96,7 @@ def information(msg):
             # 如果是分享的文件被撤回了，那么就将分享的url加在msg_body中发送给文件助手
             if old_msg['msg_type'] == "Sharing":
                 msg_body += "\n就是这个链接➣ " + old_msg.get('msg_share_url')
-            print(msg_body+'hhhhhh')
-            fw.write(msg_body+'\n')
+            fw.write(msg_body + '\n')
             fw.flush()
             # 将撤回消息发送到文件助手
             itchat.send_msg(msg_body, toUserName='filehelper')
@@ -89,7 +110,8 @@ def information(msg):
                 os.remove(old_msg['msg_content'])
             # 删除字典旧消息
             msg_information.pop(old_msg_id)
-
+    # 在好友列表中查询发送信息的好友昵称
+    msg_from = itchat.search_friends(userName=msg['FromUserName'])['NickName']
 
 @itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD, MAP,
                       SHARING, RECORDING, ATTACHMENT,
@@ -110,10 +132,18 @@ def handle_receive_msg(msg):
     msg_share_url = None  # 储存分享的链接，比如分享的文章和音乐
     print(msg['Type'])
     print(msg['MsgId'])
+
     # 如果发送的消息是文本或者好友推荐
     if msg['Type'] == 'Text' or msg['Type'] == 'Friends':
         msg_content = msg['Text']
         print(msg_content)
+        if (msg['FromUserName'] != itchat.originInstance.storageClass.userName) and (analyze(msg_content, 0)):
+            # print(msg_from)
+            itchat.send(u'您可能在和“%s”的聊天中被询问私人信息，请注意防范 聊天内容为：\n\n%s' %
+                        (msg_from, msg['Text']), toUserName='filehelper')
+        elif (msg['FromUserName'] == itchat.originInstance.storageClass.userName) and (ifPersonalInfo(msg_content)):
+            itchat.send(u'您可能在和“%s”的聊天中泄露了私人信息，请注意防范 聊天内容为：\n\n%s\n\n请及时撤回' %
+                        (msg_from, msg['Text']), toUserName='filehelper')
     # 如果发送的消息是附件、视屏、图片、语音
     elif msg['Type'] == "Attachment" or msg['Type'] == "Video" \
             or msg['Type'] == 'Picture' \
@@ -144,6 +174,7 @@ def handle_receive_msg(msg):
         msg_share_url = msg['Url']  # 记录分享的url
         print(msg_share_url)
     face_bug = msg_content
+
     ##将信息存储在字典中，每一个msg_id对应一条信息
     msg_information.update(
         {
@@ -154,6 +185,8 @@ def handle_receive_msg(msg):
             }
         }
     )
+
+
 
 ##这个是用于监听是否有Group消息撤回
 @itchat.msg_register(NOTE, isGroupChat=True, isMpChat=True)
@@ -175,8 +208,8 @@ def information(msg):
             # 如果是分享的文件被撤回了，那么就将分享的url加在msg_body中发送给文件助手
             if old_msg['msg_type'] == "Sharing":
                 msg_body += "\n就是这个链接➣ " + old_msg.get('msg_share_url')
-            # 将撤回消息保存到文件中
             fw.write(msg_body + '\n')
+            fw.flush()
             # 将撤回消息发送到文件助手
             itchat.send_msg(msg_body, toUserName='filehelper')
             # 有文件的话也要将文件发送回去
@@ -185,17 +218,14 @@ def information(msg):
                     or old_msg["msg_type"] == "Video" \
                     or old_msg["msg_type"] == "Attachment":
                 file = '@fil@%s' % (old_msg['msg_content'])
-                fw.write(msg_body + '\n')
                 itchat.send(msg=file, toUserName='filehelper')
                 os.remove(old_msg['msg_content'])
             # 删除字典旧消息
             msg_information.pop(old_msg_id)
-
-
-
-
 """ 数据统计分析函数 """
 fdata = codecs.open('message.json','w','utf8')
+friendlist = codecs.open('friendlist.txt', 'w', 'utf-8')
+roomlist = codecs.open('roomlist.txt', 'w', 'utf-8')
 """ 好友地区分布 """
 def friends_area(friends):
     provinces = []
@@ -232,7 +262,9 @@ def friend_sex(friends):
     return male,female,other
 
 if __name__ == '__main__':
-    fw = codecs.open('recall.txt', 'w', 'utf-8')
+    # itchat.auto_login( hotReload=True)
+    # itchat.run()
+    fw = codecs.open('recall.txt', 'a', 'utf-8')
     itchat.auto_login( hotReload=True)
     friends = itchat.get_friends(update=True)[0:]
     s0, s1, s2 = friend_sex(friends)
@@ -243,7 +275,18 @@ if __name__ == '__main__':
           'c1': c1,
           'c2': c2
           }
+    # print(data)
     json.dump(data, fdata)
+    fdata.close()
+    friendlist.write( '您总共有' + str(len(friends)-1)+'位好友' + '\n')
+    for i in friends[1:]:
+        friendlist.write(i['NickName']+' is your friend now.'+'\n')
+    friendlist.close()
+    """ 获取群聊列表 """
+    rooms = itchat.get_chatrooms()
+    roomlist.write('您总共有' + str(len(rooms)) + '个群，群名称列表如下：' + '\n')
+    for i in rooms:
+        roomlist.write(i['NickName'] + '\n')
+    roomlist.close()
     itchat.run()
     fw.close()
-
